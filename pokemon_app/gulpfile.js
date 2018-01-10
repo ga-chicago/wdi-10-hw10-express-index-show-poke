d142d84ec508f02ffadef1b44f3f27a3af0db2e8
Anthony <anthonyjlower@gmail.com>
2018-01-10 11:15:12 -0600

    Commit 11 - added gulp and configured it to compile LESS

---
+++ pokemon_app/gulpfile.js
@@ -0,0 +1,14 @@
+const gulp = require("gulp");
+const less = require("gulp-less");
+
+gulp.task("less", () => {
+	gulp.src("./less/style.less")
+		.pipe(less())
+		.pipe(gulp.dest("./public/css/"))
+})
+
+gulp.task("watch", () => {
+	gulp.watch("./less/*.less", ['less'])
+})
+
+gulp.task("default", ["less", "watch"])
