b128b4903c850c7ff01c283e0474f0989dc6d670
Charles <because@charlesis.me>
2018-01-10 16:04:42 -0600

    I can view a list of all my pokemon in the browser

--- server.js
+++ server.js
@@ -2,16 +2,40 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const pokemon = require("./models/pokemon.js")
+const bodyParser = require("body-parser")
 
+//Middleware
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
+app.use((req, res, next) => {
+    next()
+})
+
+//serves up static files, CSS, client javascript, all that jazz.
+app.use(express.static("public"));
+
+// Server is running at this port
 app.listen(port, function () {
     console.log("Server is runnin' on port 3000")
 });
 
-app.get ("/" , (req,res) => {
+//Root route...get it?
+app.get("/", (req, res) => {
     res.send("Welcome to the Pokemon App!")
 })
 
-app.get ("/pokemon/", (req,res) => {
-    res.render("index.ejs")
+//This is the landing page I want them to go to 
+app.get("/pokemon/", (req, res) => {
+    res.render("index.ejs", {
+        pokemon: pokemon
+    })
+//    console.log(pokemon)
+})
+
+//pulls data from array and places it on the webpage
+app.get("/pokemon/:id", (req, res) => {
+   
+//    console.log(pokemon[req.params.id])
 })
