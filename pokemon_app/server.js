43307abfb5ed264aa6c46d4670ace20f8a264b1a
Sierra Moore <sierra.v.moore@gmail.com>
2018-01-09 22:36:38 -0600

    Commit 9 - created show views of each pokemon

--- pokemon_app/server.js
+++ pokemon_app/server.js
@@ -4,10 +4,12 @@ const port = 3000;
 
 const pokemon = require("./models/pokemon.js");
 
+//Route to homepage
 app.get('/', (req, res) =>{
     res.send('Welcome to the Pokemon App!');
 });
 
+//Route to pokemon list
 app.get('/pokemon', (req, res) =>{
     res.render('index.ejs', {
         //pass entire array
@@ -15,15 +17,25 @@ app.get('/pokemon', (req, res) =>{
     });
 });
 
+//--Route to pokemon index in arr number--
+// app.get('/pokemon/:id', (req, res) =>{
+//     res.send(req.params.id);
+// });
 
-app.get('/pokemon/:id', (req, res) =>{
-    res.send(req.params.id);
+//Route to show chosen pokemon with name and img
+app.get('/pokemon/:id', (req, res) => {
+    res.render('show.ejs', {
+        pokemon: pokemon[req.params.id],
+    });
+
+    res.redirect('/pokemon')
 });
 
-// app.get('/pokemon/:id', (req, res) => {
-//     res.render('show.ejs', {
-//         pokemon: pokemon[req.params.id]
-//     });
+
+
+
+
+
 
 
 
