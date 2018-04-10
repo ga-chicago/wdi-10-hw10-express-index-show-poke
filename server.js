9a4f0b88977ba106626f25d0f44c109ebf917237
Noemi Romero <nlll02@yahoo.com>
2018-04-10 17:01:26 -0500

    Commit 4 - Connected my database, can see json in the browser

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const Port = 3000;
 app.get('/', (req, res) => {
 	res.send('Welcome to the Pokemon App!');
 })
-
+const pokemon = require('./models/pokemon.js')
+app.get('/pokemon', (req, res) => {
+	res.send(pokemon)
+})
 
 
 
