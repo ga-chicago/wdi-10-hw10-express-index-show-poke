c5cc33a7870d6a1487233c554e039a21207a9f0c
Tyler Earls <tyler.a.earls@gmail.com>
2018-04-10 16:59:05 -0500

    connected my database, can see json in the browser

--- pokemon_app/server.js
+++ pokemon_app/server.js
@@ -4,11 +4,20 @@ const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const PORT = 3000;
 
+const pokemon = require('./models/pokemon.js')
+
 app.get('/', (req, res) => {
 	res.send("Welcome to the Pokemon App!");
-})
+});
 
+// **index route**
+app.get('/pokemon', (req, res) => {
+	res.send(pokemon);
+	// res.render('index.ejs', {
+	// 	thePokemon: pokemon
+	// });
+});
 
 app.listen(PORT, () => {
 	console.log("server listening on port 3000");
-})
+});
