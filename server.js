e8d9454d0bc9a53b2d5438d889c7d09dce1c14b8
Noemi Romero <nlll02@yahoo.com>
2018-04-10 18:51:13 -0500

    Commit 9 - created show views of each pokemon

--- server.js
+++ server.js
@@ -21,7 +21,10 @@ app.get('/pokemon', (req, res) => {
 })
 
 app.get('/pokemon/:id', (req, res) => {
-	res.send(req.params.id);
+	res.render('show.ejs', {
+		poke: pokemon[req.params.id]
+	});
+	
 })
 
 
