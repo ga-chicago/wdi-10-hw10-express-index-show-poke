5337ea2cccf5e849df2291ca92e87a4822ecfb8b
Tyler Earls <tyler.a.earls@gmail.com>
2018-04-10 21:47:07 -0500

    show view shows req.params.id

--- pokemon_app/server.js
+++ pokemon_app/server.js
@@ -20,6 +20,11 @@ app.get('/pokemon', (req, res) => {
 	});
 });
 
+// **show route**
+app.get('/pokemon/:id', (req, res) => {
+	res.send(req.params.id);
+})
+
 app.listen(PORT, () => {
 	console.log("server listening on port 3000");
 });
