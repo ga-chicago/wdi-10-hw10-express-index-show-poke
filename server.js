e0691fe39c49fb65233d650fd56b6a961d49afb6
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-04-11 08:21:13 -0500

    Connected Server, See JSON in Browser

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const pokemon = require('./models/pokemon.js')
 
 
 app.get('/', (req, res)=>{
@@ -8,6 +9,9 @@ app.get('/', (req, res)=>{
 })
 
 
+app.get('/pokemon', (req, res)=>{
+	res.send(pokemon);
+})
 
 
 
